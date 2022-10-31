f85c105b67c2ba5de48b0b6f21268c32404ca063
rail <mr1204nicola@gmail.com>
2022-10-31 13:20:38 +0400

    firebase storage upload function added when user click the complete registration button, profile image will upload to server /Users/userID/

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import database from '@react-native-firebase/database';
-import storage from '@react-native-firebase/storage';
 import {
   SafeAreaView,
   Image,
@@ -10,13 +9,29 @@ import {
   Text,
   Alert,
   Platform,
+  View,
 } from 'react-native';
 import DatePicker from 'react-native-date-picker';
+import storage from '@react-native-firebase/storage';
+import utils from '@react-native-firebase/app';
+
 import {launchImageLibrary} from 'react-native-image-picker';
+import LinearGradient from 'react-native-linear-gradient';
 
 const App = () => {
+  //this function will upload the profile picture of student when select it
+
+  const uploadImage = image => {
+    const reference = storage().ref(`Users/${userID}/test.png`);
+    reference.putFile(image).then(() => {
+      console.log('image uploaded successfully...');
+    });
+    reference.putFile(image).catch(error => {
+      console.log('Something happend while image upload process: ' + error);
+    });
+  };
+
   const [open, setOpen] = useState(false);
-  let a = new Date();
 
   let userID = 10001;
 
@@ -25,11 +40,9 @@ const App = () => {
   let [fathername, setFathername] = useState('');
   const [birthday, setBirthday] = useState();
   const [birthdayText, setBirthdayText] = useState('Doğum tarixini seçin: ');
-  let [school, setSchool] = useState('eeeeef');
-  let [home, setHome] = useState('eeeeef');
-  let [profilePicture, setProfilePicture] = useState(
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmsSvEY1mmpXAuHIuVwPUsHQJ6DdzgP3tyJw&usqp=CAU',
-  );
+  let [school, setSchool] = useState('20 nömrəli məktəb');
+  let [home, setHome] = useState('Ev ünvanını seçin');
+  let [profilePicture, setProfilePicture] = useState('');
   let registrationDate = new Date().toLocaleString();
   let deviceDetails = {
     os: Platform.OS,
@@ -80,21 +93,15 @@ const App = () => {
       reference.once('value').then(snapshot => {
         userID = snapshot.numChildren() + 10001;
 
-        const reference = storage().ref('/Users/profilePictures');
-        async () => {
-          // path to existing file on filesystem
-          const pathToFile = profilePicture;
-          // uploads file
-          await reference
-            .putFile(pathToFile)
-            .then(console.log('uploaded'), console.log('rejected'));
-        };
+        uploadImage(profilePicture);
 
         //save new user to database
         database()
           .ref('Users/' + userID)
           .set(userData)
-          .then(console.log(`New user ${userID} is data saved!`));
+          .then(() => {
+            console.log(`New user ${userID} is data saved!`);
+          });
       });
     } else {
       if (name === '' || name.length < 3) {
@@ -116,62 +123,164 @@ const App = () => {
   };
 
   return (
-    <SafeAreaView style={styles.background}>
-      <TouchableOpacity
-        onPress={() => {
-          selectPicture();
+    <SafeAreaView style={{flex: 1}}>
+      <View
+        style={{
+          height: '50%',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          width: '100%',
         }}>
-        <Image style={styles.profileImage} source={{uri: profilePicture}} />
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            zIndex: 2,
+            elevation: Platform.OS === 'android' ? 50 : 0,
 
-      <TextInput
-        style={styles.textInput}
-        placeholder="Adı:"
-        onChangeText={text => {
-          setName(text);
-        }}
-      />
-      <TextInput
-        style={styles.textInput}
-        placeholder="Soyadı:"
-        onChangeText={text => {
-          setSurname(text);
-        }}
-      />
-      <TextInput
-        style={styles.textInput}
-        placeholder="Ata adı:"
-        onChangeText={text => {
-          setFathername(text);
-        }}
-      />
-      <TouchableOpacity onPress={() => setOpen(true)}>
-        <Text style={{fontSize: 20, color: 'white'}}>{birthdayText}</Text>
-        <DatePicker
-          modal
-          mode="date"
-          open={open}
-          date={new Date()}
-          confirmText={'Seç'}
-          cancelText={'Bağla'}
-          textColor={'orange'}
-          onConfirm={date => {
-            let customDateFormat =
-              date.getDate() +
-              '-' +
-              (date.getMonth() + 1) +
-              '-' +
-              date.getFullYear();
-            setBirthday(customDateFormat);
-            setBirthdayText(customDateFormat);
+            backgroundColor: '#BFACE0',
+            borderRadius: 100,
+          }}
+          onPress={() => {
+            selectPicture();
+          }}>
+          <Image
+            style={styles.profileImage}
+            source={
+              profilePicture === ''
+                ? require('./src/images/camera.png')
+                : {uri: profilePicture}
+            }
+          />
+        </TouchableOpacity>
+        <LinearGradient
+          colors={['#BFACE0', '#A084CA', '#645CAA']}
+          style={styles.background}>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'column',
+                zIndex: 1,
+                elevation: Platform.OS === 'android' ? 20 : 0,
+              }}>
+              <TextInput
+                style={styles.textInput}
+                placeholder="Ad:"
+                onChangeText={text => {
+                  setName(text);
+                }}
+              />
+              <TextInput
+                style={styles.textInput}
+                placeholder="Soyad:"
+                onChangeText={text => {
+                  setSurname(text);
+                }}
+              />
+              <TextInput
+                style={styles.textInput}
+                placeholder="Ata adı:"
+                onChangeText={text => {
+                  setFathername(text);
+                }}
+              />
+            </View>
+            <View
+              style={{
+                flex: 1,
+                height: '100%',
+                justifyContent: 'flex-start',
+                alignItems: 'flex-end',
+                paddingTop: 10,
+              }}>
+              <TouchableOpacity onPress={() => setOpen(true)}>
+                <Text style={styles.birthdayText}>{birthdayText}</Text>
 
-            setOpen(false);
-          }}
-          onCancel={() => {
-            setOpen(false);
-          }}
-        />
-      </TouchableOpacity>
+                <DatePicker
+                  modal
+                  mode="date"
+                  open={open}
+                  date={new Date()}
+                  confirmText={'Seç'}
+                  cancelText={'Bağla'}
+                  textColor={'orange'}
+                  onConfirm={date => {
+                    let customDateFormat =
+                      date.getDate() +
+                      '-' +
+                      (date.getMonth() + 1) +
+                      '-' +
+                      date.getFullYear();
+                    setBirthday(customDateFormat);
+                    setBirthdayText(customDateFormat);
+
+                    setOpen(false);
+                  }}
+                  onCancel={() => {
+                    setOpen(false);
+                  }}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+
+              width: '100%',
+              padding: 15,
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}>
+            <View
+              style={{
+                flex: 1,
+                maxWidth: '40%',
+                justifyContent: 'center',
+                alignItems: 'flex-start',
+              }}>
+              <View
+                style={{
+                  flex: 1,
+                  width: '100%',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Image
+                  source={require('./src/images/address.png')}
+                  style={{width: 50, height: 50}}
+                />
+                <Text style={{fontSize: 14, color: 'white'}}>{home}</Text>
+              </View>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                maxWidth: '40%',
+                justifyContent: 'center',
+                alignItems: 'flex-end',
+              }}>
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: 'column',
+                  width: '100%',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Image
+                  source={require('./src/images/school.png')}
+                  style={{width: 50, height: 50}}
+                />
+                <Text style={{fontSize: 14, color: 'white'}}>{school}</Text>
+              </View>
+            </View>
+          </View>
+        </LinearGradient>
+      </View>
+
       <TouchableOpacity
         style={styles.btnComplete}
         onPress={() => {
@@ -186,32 +295,76 @@ export default App;
 
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: 'orange',
     flex: 1,
-    opacity: 0.8,
+    flexDirection: 'column',
+    position: 'absolute',
+    top: 100,
+    zIndex: 1,
+    width: '100%',
+    height: 'auto',
+    paddingRight: 5,
+    paddingBottom: 5,
+    paddingTop: 60,
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
+    borderWidth: 0,
+    borderRadius: 20,
+    shadowColor: '#645CAA',
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.48,
+    shadowRadius: 11.95,
+    elevation: 10,
   },
   profileImage: {
     width: 150,
     height: 150,
     borderRadius: 100,
-    borderWidth: 2,
-    borderColor: 'white',
-    marginVertical: 20,
+
+    borderWidth: 1,
+    shadowColor: '#645CAA',
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.48,
+    shadowRadius: 11.95,
+    elevation: 10,
   },
   textInput: {
-    width: 200,
-    fontsize: 20,
+    width: 180,
+    borderTopRightRadius: 20,
+    borderBottomRightRadius: 20,
+    borderColor: '#645CAA',
+    fontsize: 16,
+    alignSelf: 'center',
     color: 'black',
     fontWeight: 'bold',
     backgroundColor: 'white',
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: 'white',
-    padding: 10,
-    marginVertical: 10,
+    paddingHorizontal: 15,
+    marginVertical: 5,
     maxLength: 14,
+    shadowColor: '#ffffff',
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.48,
+    shadowRadius: 11.95,
+    elevation: 15,
+  },
+  birthdayText: {
+    fontSize: 16,
+    color: 'black',
+    backgroundColor: 'white',
+    borderColor: '#645CAA',
+    width: 180,
+    borderTopLeftRadius: 20,
+    borderBottomLeftRadius: 20,
+    padding: 5,
+    paddingHorizontal: 10,
   },
   btnComplete: {
     position: 'absolute',
@@ -222,7 +375,7 @@ const styles = StyleSheet.create({
     borderColor: 'white',
     borderWidth: 2,
     borderRadius: 10,
-    backgroundColor: 'transparent',
+    backgroundColor: 'black',
     elevation: 20,
     shadowColor: 'white',
     shadowOffset: 10,
